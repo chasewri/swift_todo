8afa161be8c7de26fc4ee0f1d0aeddb1248c773f
chase-cove <chase@cove.is>
2023-01-21 11:39:26 -0700

    Initial Commit

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Chase Wright on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
